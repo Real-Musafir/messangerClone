25e6bf678234acca3e3da9329c1f61face27d985
Al Shahadath <shahadath@Als-MacBook-Air.local>
2022-02-07 21:49:58 +0600

    implement react-bootstrap & node-scss

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,16 @@
-import "./App.css";
+import "./App.scss";
+import { Container, Row, Col } from "react-bootstrap";
 
 function App() {
-  return <div>Hello React </div>;
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <h1>Register</h1>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default App;
