6b5238e13dd363f990efe62fd7deda44c5cde042
Al Shahadath <shahadath@Als-MacBook-Air.local>
2022-02-09 07:27:59 +0600

    implement react-router

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,22 @@ import "./App.scss";
 import { Container } from "react-bootstrap";
 import Register from "./pages/Register";
 import ApolloProvider from "./ApolloProvider";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <ApolloProvider>
-      <Container className="pt-5">
-        <Register />
-      </Container>
+      <BrowserRouter>
+        <Container className="pt-5">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/register" component={Register} />
+            <Route path="/login" component={Login} />
+          </Switch>
+        </Container>
+      </BrowserRouter>
     </ApolloProvider>
   );
 }
