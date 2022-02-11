ef912075d81220aecb6d74803ebab0881cc2ea4a
Al Shahadath <shahadath@Als-MacBook-Air.local>
2022-02-11 15:13:27 +0600

    setting useContext for manage state

--- client/src/App.js
+++ client/src/App.js
@@ -6,19 +6,22 @@ import ApolloProvider from "./ApolloProvider";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import { AuthProvider } from "./context/auth";
 
 function App() {
   return (
     <ApolloProvider>
-      <BrowserRouter>
-        <Container className="pt-5">
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/register" component={Register} />
-            <Route path="/login" component={Login} />
-          </Switch>
-        </Container>
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <Container className="pt-5">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/register" component={Register} />
+              <Route path="/login" component={Login} />
+            </Switch>
+          </Container>
+        </BrowserRouter>
+      </AuthProvider>
     </ApolloProvider>
   );
 }
---
+++ client/src/context/auth.js
@@ -0,0 +1,34 @@
+import React, { createContext, useReducer, useContext } from "react";
+const AuthStateContext = createContext();
+const AuthDispatchContext = createContext();
+
+const authReducer = (state, action) => {
+  switch (action.type) {
+    case "LOGIN":
+      return {
+        ...state,
+        user: action.payload,
+      };
+    case "LOGOUT":
+      return {
+        ...state,
+        user: null,
+      };
+    default:
+      throw new Error(`Unknown action type: ${action.type}`);
+  }
+};
+
+export const AuthProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(authReducer, { user: null });
+  return (
+    <AuthDispatchContext.Provider value={dispatch}>
+      <AuthStateContext.Provider value={state}>
+        {children}
+      </AuthStateContext.Provider>
+    </AuthDispatchContext.Provider>
+  );
+};
+
+export const useAuthState = () => useContext(AuthStateContext);
+export const useAuthDisptach = () => useContext(AuthDispatchContext);
