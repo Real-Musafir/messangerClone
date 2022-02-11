f36e785e6dd1a85103cc086112b20387a771dc01
Al Shahadath <shahadath@Als-MacBook-Air.local>
2022-02-11 18:52:03 +0600

    remove Old token if it is expired

--- client/src/context/auth.js
+++ client/src/context/auth.js
@@ -1,7 +1,22 @@
 import React, { createContext, useReducer, useContext } from "react";
+import jwtDecode from "jwt-decode";
+
 const AuthStateContext = createContext();
 const AuthDispatchContext = createContext();
 
+let user;
+const token = localStorage.getItem("token");
+if (token) {
+  const decodedToken = jwtDecode(token);
+  const expiresAt = new Date(decodedToken.exp * 1000);
+  if (new Data() > expiresAt) {
+    localStorage.removeItem("token");
+  } else {
+    user = decodedToken;
+  }
+} else {
+  console.log("No token found");
+}
 const authReducer = (state, action) => {
   switch (action.type) {
     case "LOGIN":
@@ -11,6 +26,7 @@ const authReducer = (state, action) => {
         user: action.payload,
       };
     case "LOGOUT":
+      localStorage.removeItem("token");
       return {
         ...state,
         user: null,
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,29 @@
 import React from "react";
+import { Row, Col, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { useAuthDisptach } from "../context/auth";
 
-export default function Home() {
+export default function Home({ history }) {
+  const dispatch = useAuthDisptach();
+
+  const logout = () => {
+    dispatch({ type: "LOGOUT" });
+    history.push("/login");
+  };
+
   return (
-    <div>
-      <h1>Home page</h1>
-    </div>
+    <Row className="bg-white justify-content-around">
+      <Link to="/login">
+        <Button variant="link">Login</Button>
+      </Link>
+
+      <Link to="/register">
+        <Button variant="link">Register</Button>
+      </Link>
+
+      <Button variant="link" onClick={logout}>
+        Logout
+      </Button>
+    </Row>
   );
 }
