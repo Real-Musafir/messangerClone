3a7ca6d4beb59a603dba5c99f1405485b1a57692
Al Shahadath <shahadath@Als-MacBook-Air.local>
2022-02-19 09:54:42 +0600

    fix logout problem when login new user

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -13,6 +13,7 @@ export default function Home({ history }) {
   const logout = () => {
     dispatch({ type: "LOGOUT" });
     history.push("/login");
+    window.location.href = "/login";
   };
 
   return (
